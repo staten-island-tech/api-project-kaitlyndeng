1702ab35f2e723c21af9d437745fe35719cdfa44
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-01-06 10:14:56 -0500

    Commit 3

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+const URL = "https://www.fishwatch.gov/api/species";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
