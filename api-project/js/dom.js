99069848e124fb41fef051e1408a7dd085f06e41
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-01-13 10:14:54 -0500

    Commit 7

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,1 +1,4 @@
-
+const DOMSelectors = {
+  box: document.getElementById("box"),
+  all: document.getElementById("all"),
+};
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,6 +1,6 @@
 import "../styles/style.css";
 
-let species = "red snapper";
+let species = "atlantic-cod";
 const URL = `https://www.fishwatch.gov/api/species/${species}`;
 async function getData(URL) {
   try {
@@ -9,7 +9,7 @@ async function getData(URL) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      document.getElementById("api-response").textContent = data.data;
+      document.getElementById("api-response").textContent = data.content;
 
       console.log("good");
     }
@@ -22,13 +22,41 @@ getData(URL);
 
 function explain(species) {
   const explainPromise = new Promise(function (resolve, reject) {
-    resolve(`This is a ${species}.`);
+    resolve(`This is the ${species}.`);
   });
   return explainPromise;
 }
 
-const redsnapper = explain("red snapper");
+const redsnapper = explain("atlantic cod");
 
 redsnapper.then((result) => {
   console.log(result);
 });
+
+// const listFish = species.forEach((species) => {
+//   StartPg(species);
+// });
+// listFish();
+
+DOMSelectors.form.addEventlistener("submit", function all(e) {
+  e.preventDefault();
+  console.log(e);
+  clear();
+  function card() {
+    let category = DOMSelectors.category.value;
+  }
+  function insert() {
+    species.forEach((species) =>
+      DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
+      <h2 class="name"></h2>
+      <img class="image" src=""/>
+      <h3 class="funfact"></h3>
+      </div>`
+      )
+    );
+  }
+});
+card();
+insert();
