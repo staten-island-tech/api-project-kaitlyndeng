36606c6e1ed78e2f22ec05476ba5d4f82a7ae837
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-03-14 10:03:31 -0400

    Commit 18

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,42 +1,41 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-const url = "https://api.disneyapi.dev/characters";
+const url = "https://api.disneyapi.dev/characters?pageSize=7450";
 
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    if (response.status < 200 || response.status > 299) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      console.log(data);
-      console.log("good");
-    }
+// async function getData(url) {
+//   try {
+//     const response = await fetch(url);
+//     if (response.status < 200 || response.status > 299) {
+//       throw new Error(response);
+//     } else {
+//       const data = await response.json();
+//       console.log(data);
+//       console.log("good");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     console.log("bad");
+//   }
+// }
 
-  } catch (error) {
-    console.log(error);
-    console.log("bad");
-  }
-}
-
-getData(url);
+// getData(url);
 
 DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log(e);
   let userInput = DOMSelectors.userInput.value;
 
-async function specificCharacter(url, userInput) {
-    try{
-      const response = await fetch(url);
+  async function specificCharacter(url, userInput) {
+    try {
+      const response = await fetch(url, userInput);
       const data = await response.json();
-      data.data
-      .filter((element) => element.name.includes(`${userInput}`))
-      .map((element) => {
-        DOMSelectors.box.insertAdjacentHTML(
-          "beforeend",
-          `<div class="card">
+      data
+        .filter((element) => element.name.includes(`${userInput}`))
+        .map((element) => {
+          DOMSelectors.box.insertAdjacentHTML(
+            "beforeend",
+            `<div class="card">
           <img class="image" src="${element.imageUrl}" alt="">
           <h2 class="name">${element.name}</h2>
           <div class="features">
@@ -50,18 +49,22 @@ async function specificCharacter(url, userInput) {
           </div>
         </div>
         `
-        )
-      })
-    }catch (error) {
+          );
+        });
+    } catch (error) {
       console.log(error);
+      DOMSelectors.box.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="box">Character not found. Please check your spelling.</div>`
+      );
     }
   }
   specificCharacter(url, userInput);
-})
+});
 
 function clear() {
   DOMSelectors.box.innerHTML = "";
   DOMSelectors.userInput.innerHTML = "";
-};
+}
 
-clear()
+clear();
