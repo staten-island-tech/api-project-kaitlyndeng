e5ae92c366bb734b406ea9d2afb13d3b41732696
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-03-17 02:29:30 -0400

    Final Updated

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,94 +1,54 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-const url = `https://api.disneyapi.dev/characters?page=${page}`;
-let page = 1;
-let userInput = DOMSelectors.userInput.value;
-do{async function getData(url) {
-  try {
-    const response = await fetch(url);
-    if (response.status < 200 || response.status > 299) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      data.data
-        .filter((element) => element.name.includes(`${userInput}`))
-        .map((element) => {
+
+DOMSelectors.button.addEventListener("click", function(){
+
+  const url = "https://thronesapi.com/api/v2/Characters";
+  let userInput = DOMSelectors.userInput.value;
+    
+    async function getData(url, userInput) {
+      
+      try{
+        const response = await fetch(url);
+        if (response.status < 200 || response.status > 299) {
+          throw new Error(response);
+       } else{
+        const data = await response.json();
+
+        const specificData = 
+        data
+        .filter((element) => element.firstName.includes(userInput));
+        specificData.map((element) => {
           DOMSelectors.box.insertAdjacentHTML(
             "beforeend",
             `<div class="card">
-          <img class="image" src="${element.imageUrl}" alt="">
-          <h2 class="name">${element.name}</h2>
-          <div class="features">
-            <ul><b>Films: </b> ${element.films}</ul>
-            <br></br>
-            <ul><b>Short Films: </b> ${element.shortFilms}</ul>
-            <br></br>
-            <ul><b>TV Shows: </b>${element.tvShows}</ul>
-            <br></br>
-            <ul><b>Video Games: </b> ${element.videoGames}</ul>
+            <img class="image" src="${element.imageUrl}" alt="">
+            <h2 class="name">${element.fullName}</h2>
+            <p>Family: ${element.family}</p>
           </div>
-        </div>
-        `
+          `
           );
-        });
+          console.log(specificData)
+        })
+        if(specificData.length === 0) {
+          throw new Error(response);
+        }
+      }
+      
+      } catch (error) {
+        console.log(error);
+        DOMSelectors.box.insertAdjacentHTML(
+          "beforeend",
+          `<div class="box">Character not found. Please check your spelling.</div>`
+        );
+      }
+    };
+  
+   
+      getData(url, userInput);
     }
-  } catch (error) {
-    console.log(error);
-    console.log("bad");
-  }
-}
-
-getData(url);}
-
-
-
-
-// async function specificCharacter() {
-//   try {
-//     for (let page = 1; page <= 149; page++) {
-//       const response = await fetch(url);
-//       const data = await response.json();
-
-//       data.data
-//         .filter((element) => element.name.includes(`${userInput}`))
-//         .map((element) => {
-//           DOMSelectors.box.insertAdjacentHTML(
-//             "beforeend",
-//             `<div class="card">
-//           <img class="image" src="${element.imageUrl}" alt="">
-//           <h2 class="name">${element.name}</h2>
-//           <div class="features">
-//             <ul><b>Films: </b> ${element.films}</ul>
-//             <br></br>
-//             <ul><b>Short Films: </b> ${element.shortFilms}</ul>
-//             <br></br>
-//             <ul><b>TV Shows: </b>${element.tvShows}</ul>
-//             <br></br>
-//             <ul><b>Video Games: </b> ${element.videoGames}</ul>
-//           </div>
-//         </div>
-//         `
-//           );
-//         });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     DOMSelectors.box.insertAdjacentHTML(
-//       "afterbegin",
-//       `<div class="box">Character not found. Please check your spelling.</div>`
-//     );
-//   }
-// }
-// specificCharacter();
-
-DOMSelectors.form.addEventListener(
-  "submit",
-  async function specificCharacter() {
-    e.preventDefault();
-    console.log(e);
-  }
-);
+    );
 
 DOMSelectors.reset.addEventListener("click", function () {
   DOMSelectors.box.innerHTML = "";
