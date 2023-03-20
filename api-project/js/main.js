0e58acaac687ea8bd0d95e818d0e4189db735bd5
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-03-20 00:51:21 -0400

    Commit Final

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,9 +1,40 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
+const api = "https://thronesapi.com/api/v2/Characters";
+async function getAll(api) {
+  try {
+    const response = await fetch(api);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      data.forEach((element) => {
+        DOMSelectors.box.insertAdjacentHTML(
+          "beforeend",
+         `<div class="card">
+         <img class="image" src="${element.imageUrl}" alt="Photo: ${element.fullName}">
+         <h2 class="name">${element.fullName}</h2>
+         <h3>Family: ${element.family}</h3>
+        </div>`
+        )
+      }
+       
+      )
+
+      console.log("good");
+      }
+   } catch (error) {}
+ }
+getAll(api);
+
+function clear(){
+  DOMSelectors.box.innerHTML=""
+} 
+
 
 DOMSelectors.button.addEventListener("click", function(){
-
+  clear();
   const url = "https://thronesapi.com/api/v2/Characters";
   let userInput = DOMSelectors.userInput.value;
     
@@ -16,6 +47,8 @@ DOMSelectors.button.addEventListener("click", function(){
        } else{
         const data = await response.json();
 
+
+
         const specificData = 
         data
         .filter((element) => element.firstName.includes(userInput));
@@ -25,7 +58,7 @@ DOMSelectors.button.addEventListener("click", function(){
             `<div class="card">
             <img class="image" src="${element.imageUrl}" alt="">
             <h2 class="name">${element.fullName}</h2>
-            <p>Family: ${element.family}</p>
+            <h3>Family: ${element.family}</h3>
           </div>
           `
           );
